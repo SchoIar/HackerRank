58bd2e25835496d7957ec75c7586d3392c4d4178
Anton <antonilic7@gmail.com>
2022-12-05 09:06:34 -0800

    code for Day6

--- 30 Days Of Code/day6/review.cpp
+++ 30 Days Of Code/day6/review.cpp
@@ -5,16 +5,30 @@
 #include <algorithm>
 using namespace std;
 
-
-int main() {
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+int main()
+{
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
     int numOfTestCases;
+    string testCase1;
     cin >> numOfTestCases;
+    for (int j = 0; j < numOfTestCases; j++)
+    {
+        cin >> testCase1;
 
-    //read data from two strings here
+        // read data from two strings here
+        for (int i = 0; i < testCase1.length(); i += 2)
+        {
+            cout << testCase1[i]; // priting odd
+        }
+        cout << " ";
+        for (int i = 1; i < testCase1.length(); i += 2)
+        {
+            cout << testCase1[i]; // priting odd
+        }
+        // output data from both strings here, in [EVEN indicies] [ODD indicies]\n
 
-    //output data from both strings here, in [EVEN indicies] [ODD indicies]\n
-    
-    
+        cout << '\n';
+    }
+
     return 0;
 }
