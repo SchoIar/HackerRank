7a70dbb1bc598efa59bcaad59f5f1d1207af45ff
Anton <antonilic7@gmail.com>
2022-12-22 21:46:30 -0800

    linked lists

--- 30 Days Of Code/day14/main.cpp
+++ 30 Days Of Code/day14/main.cpp
@@ -6,51 +6,58 @@
 
 using namespace std;
 
-class Difference {
-    private:
+class Difference
+{
+private:
     vector<int> elements;
-  
-  	public:
-  	int maximumDifference;
-      Difference( vector<int> listOfStuff){
-          this->elements = listOfStuff;
-      }
 
-    void computeDifference(){
+public:
+    int maximumDifference;
+    Difference(vector<int> listOfStuff)
+    {
+        this->elements = listOfStuff;
+    }
+
+    void computeDifference()
+    {
         int numberOfElements = elements.size();
-        //comparing ABS of both numbers
+        // comparing ABS of both numbers
         maximumDifference = 0;
-        for(int i = 0; i<numberOfElements; i++){
-            for(int j = 0; j<numberOfElements; j++){
-                if(abs(elements[i]-elements[j]) > maximumDifference){
-                    maximumDifference = abs(elements[i]-elements[j]);
+        for (int i = 0; i < numberOfElements; i++)
+        {
+            for (int j = 0; j < numberOfElements; j++)
+            {
+                if (abs(elements[i] - elements[j]) > maximumDifference)
+                {
+                    maximumDifference = abs(elements[i] - elements[j]);
                 }
             }
         }
-        
     }
-	// Add your code here
+    // Add your code here
 
 }; // End of Difference class
 
-int main() {
+int main()
+{
     int N;
     cin >> N;
-    
+
     vector<int> a;
-    
-    for (int i = 0; i < N; i++) {
+
+    for (int i = 0; i < N; i++)
+    {
         int e;
         cin >> e;
-        
+
         a.push_back(e);
     }
-    
+
     Difference d(a);
-    
+
     d.computeDifference();
-    
+
     cout << d.maximumDifference;
-    
+
     return 0;
 }
