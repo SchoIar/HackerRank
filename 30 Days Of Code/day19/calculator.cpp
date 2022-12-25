03418fe2fe54d332e14903633112379f818e95b2
Anton <antonilic7@gmail.com>
2022-12-24 21:18:36 -0800

    format

--- 30 Days Of Code/day19/calculator.cpp
+++ 30 Days Of Code/day19/calculator.cpp
@@ -5,17 +5,22 @@
 #include <algorithm>
 #include <string>
 using namespace std;
-class AdvancedArithmetic{
-    public:
-        virtual int divisorSum(int n)=0;
-};
-class Calculator : public AdvancedArithmetic {
+class AdvancedArithmetic
+{
 public:
-    int divisorSum(int n) {
-        //Should return sum of all divisors
+    virtual int divisorSum(int n) = 0;
+};
+class Calculator : public AdvancedArithmetic
+{
+public:
+    int divisorSum(int n)
+    {
+        // Should return sum of all divisors
         int sumOfOtherDivisors = 0;
-        for(int i = 1; i <= n; i++){
-            if(n%i == 0){
+        for (int i = 1; i <= n; i++)
+        {
+            if (n % i == 0)
+            {
                 sumOfOtherDivisors += i;
             }
         }
@@ -23,11 +28,13 @@ public:
     }
 };
 
-int main(){
+int main()
+{
     int n;
     cin >> n;
-    AdvancedArithmetic *myCalculator = new Calculator(); 
+    AdvancedArithmetic *myCalculator = new Calculator();
     int sum = myCalculator->divisorSum(n);
-    cout << "I implemented: AdvancedArithmetic\n" << sum;
+    cout << "I implemented: AdvancedArithmetic\n"
+         << sum;
     return 0;
 }
