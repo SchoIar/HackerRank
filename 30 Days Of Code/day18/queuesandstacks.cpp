119a37863e068e2bbd9d7a06987fb282cf5997c4
Anton <antonilic7@gmail.com>
2022-12-24 21:01:50 -0800

    solution for d18

---
+++ 30 Days Of Code/day18/queuesandstacks.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <queue>
+#include <stack>
+using namespace std;
+
+class Solution {
+private:
+    stack<char> myStack;
+    queue<char> myQueue;
+    
+public:
+    //Write your code here
+    void pushCharacter(char selectedChar){
+        myStack.push(selectedChar);
+    }
+    
+    void enqueueCharacter(char selectedChar){
+        myQueue.push(selectedChar);
+    }
+    
+    char popCharacter(){
+        char temp = myStack.top();
+        myStack.pop();
+        return temp;
+        
+    }
+    
+    char dequeueCharacter(){
+        char temp = myQueue.front();
+        myQueue.pop();
+        return temp;
+    }
+};
+
+int main() {
+    // read the string s.
+    string s;
+    getline(cin, s);
+    
+  	// create the Solution class object p.
+    Solution obj;
+    
+    // push/enqueue all the characters of string s to stack.
+    for (int i = 0; i < s.length(); i++) {
+        obj.pushCharacter(s[i]);
+        obj.enqueueCharacter(s[i]);
+    }
+    
+    bool isPalindrome = true;
+    
+    // pop the top character from stack.
+    // dequeue the first character from queue.
+    // compare both the characters.
+    for (int i = 0; i < s.length() / 2; i++) {
+        if (obj.popCharacter() != obj.dequeueCharacter()) {
+            isPalindrome = false;
+            
+            break;
+        }
+    }
+    
+    // finally print whether string s is palindrome or not.
+    if (isPalindrome) {
+        cout << "The word, " << s << ", is a palindrome.";
+    } else {
+        cout << "The word, " << s << ", is not a palindrome.";
+    }
+    
+    return 0;
+}
