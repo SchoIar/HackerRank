57533cc2a25fbb356c548d31b4161ca4bd5ea9b4
Anton <antonilic7@gmail.com>
2023-05-03 20:24:03 -0700

    solution to medium problem

---
+++ Problems/Data-Structures/Arrays/Sparse-Arrays.py
@@ -0,0 +1,58 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'matchingStrings' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. STRING_ARRAY stringList
+#  2. STRING_ARRAY queries
+#
+
+def matchingStrings(stringList, queries):
+    stringDict = {}
+    for query in queries:
+        stringDict[query] = 0
+        
+    for element in stringList:
+        if element in stringDict:
+            value = stringDict[element] 
+            stringDict[element] += 1
+    
+    listOfQueriesNumbers = []
+    for query in queries:
+        listOfQueriesNumbers.append(stringDict[query])
+        
+    return listOfQueriesNumbers
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    stringList_count = int(input().strip())
+
+    stringList = []
+
+    for _ in range(stringList_count):
+        stringList_item = input()
+        stringList.append(stringList_item)
+
+    queries_count = int(input().strip())
+
+    queries = []
+
+    for _ in range(queries_count):
+        queries_item = input()
+        queries.append(queries_item)
+
+    res = matchingStrings(stringList, queries)
+
+    fptr.write('\n'.join(map(str, res)))
+    fptr.write('\n')
+
+    fptr.close()
