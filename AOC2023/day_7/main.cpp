4f31607b598000b1451e85fd76fd899a56fa5ece
Anton <antonilic7@gmail.com>
2023-12-23 19:46:27 -0800

    Debugged

--- AOC2023/day_7/main.cpp
+++ AOC2023/day_7/main.cpp
@@ -41,76 +41,68 @@ int whatType(const string &hand)
     }
 
     if (isThreeOfAKind && isPair)
+    {
         return 6; // full house
+    }
     if (isThreeOfAKind)
+    {
         return 3; // three of a kind
+    }
     if (isPair && secondMaxCount == 2)
+    {
         return 2; // two pairs
+    }
     if (isPair)
+    {
         return 1; // one pair
-    return 0;     // no pair or other hand
+    }
+    return 0; // no pair or other hand
 }
 
-// Description: Sorts cards in ascending order
-void sort(vector<string>& cards){
-    for(int i = 0; i < cards.size(); i++){
-        for(int j = i; j < cards.size(); j++){
-            string hand1 = cards[i].substr(0, hand1.find(" "));
-            string hand2 = cards[j].substr(0, hand2.find(" "));
-            if(i != j && compareHands(hand1, hand2)){ //hand1 > hand2
-                string temp = cards[j];
-                cards[j] = cards[i];
-                cards[i] = temp;
+int isFirstCharGreater(const char digitOne, const char digitTwo)
+{
+    const char cardValues[] = {'A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2'};
+    map<char, int> cardValueMap;
+    for (int i = 0; i < 13; i++)
+    {
+        cardValueMap[cardValues[i]] = i;
+    }
+
+    if (digitOne == digitTwo)
+    {
+        return -1; // equal
+    }
+
+    return cardValueMap[digitOne] < cardValueMap[digitTwo] ? 1 : 0;
+}
+
+bool compareHands(const string handOne, const string handTwo)
+{
+    for (int i = 0; i < min(handOne.length(), handTwo.length()); i++)
+    {
+        int comparisonResult = isFirstCharGreater(handOne[i], handTwo[i]);
+        if (comparisonResult != -1)
+        {
+            return comparisonResult == 1;
+        }
+    }
+    return false; // base case; cards are equal.
+}
+
+void sort(vector<string> &cards)
+{
+    for (int i = 0; i < cards.size(); i++)
+    {
+        for (int j = i; j < cards.size(); j++)
+        {
+            string hand1 = cards[i].substr(0, cards[i].find(" "));
+            string hand2 = cards[j].substr(0, cards[j].find(" "));
+            if (i != j && compareHands(hand1, hand2))
+            {
+                swap(cards[i], cards[j]);
             }
         }
     }
-}
-
-// Description: Compares card hand values. A hand is larger
-//              if the first character is larger then another hand.
-//              If characters are equal, the 2nd character is compared..
-//              Returns true if the first hand is greater.
-//              Otherwise returns false.
-bool compareHands(const string handOne, const string handTwo)
-{
-    for (int i = 0; i < handOne.length(); i++)
-    {
-        if(isFirstCharGreater(handOne[i], handTwo[i]) != -1){
-            return isFirstCharGreater(handOne[i], handTwo[i]);
-        }
-    }
-    return false; //base case; cards are equal.
-}
-
-// Description: Compares character values.
-//      Returns 1 if first digit is larger, 0 if 2nd digit is larger,
-//      -1 if they are equal.
-int isFirstCharGreater(const char digitOne, const char digitTwo)
-{
-    if (digitOne == digitTwo)
-        return -1; // equal
-
-    if (isdigit(digitOne) && isdigit(digitTwo))
-        return isdigit(digitOne) > isdigit(digitTwo);
-    else if (isdigit(digitTwo))
-    {
-        return 1;
-    }
-    else if (isdigit(digitOne))
-    {
-        return 0;
-    }
-    //else both number cards; A K Q J
-    char cardValues[4] = {'A', 'K', 'Q', 'J'};
-
-    for(int i = 0; i < 3; i ++){
-        if(digitOne == cardValues[i]){
-            return 1;
-        } else if(digitTwo == cardValues[i]){
-            return 0;
-        }
-    }
-    
 }
 
 int main()
@@ -118,7 +110,7 @@ int main()
     vector<string> fives, fours, fullhouses, threes, twos, ones, nones;
 
     string line;
-    while (getline(cin, line)) // reading input; placing it into proper bins
+    while (getline(cin, line))
     {
         string hand = line.substr(0, line.find(" "));
         int type = whatType(hand);
@@ -148,9 +140,8 @@ int main()
             break;
         }
     }
-
-    // TODO: Sort each group by strength (ascending). Start at 1s and add
-    //       product of value and bid.
+    
+    sort(nones);
 
     for (int i = 0; i < nones.size(); i++)
     {
