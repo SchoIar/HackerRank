0924987bb4c3f7099eb34aac9d7febba7e5b6613
Anton <antonilic7@gmail.com>
2023-12-23 19:41:23 -0800

    added sort

--- AOC2023/day_7/main.cpp
+++ AOC2023/day_7/main.cpp
@@ -12,7 +12,7 @@ int whatType(const string &hand)
     map<char, int> frequency;
     for (char card : hand)
     {
-        frequency[card]++;
+        frequency[card]++; // counting card frequency
     }
 
     int maxCount = 0, secondMaxCount = 0;
@@ -33,7 +33,9 @@ int whatType(const string &hand)
         else if (pair.second == 4)
         {
             return 4; // four of a kind
-        } else if (pair.second == 5){
+        }
+        else if (pair.second == 5)
+        {
             return 5; // five of a kind
         }
     }
@@ -46,7 +48,69 @@ int whatType(const string &hand)
         return 2; // two pairs
     if (isPair)
         return 1; // one pair
-    return 0; // no pair or other hand
+    return 0;     // no pair or other hand
+}
+
+// Description: Sorts cards in ascending order
+void sort(vector<string>& cards){
+    for(int i = 0; i < cards.size(); i++){
+        for(int j = i; j < cards.size(); j++){
+            string hand1 = cards[i].substr(0, hand1.find(" "));
+            string hand2 = cards[j].substr(0, hand2.find(" "));
+            if(i != j && compareHands(hand1, hand2)){ //hand1 > hand2
+                string temp = cards[j];
+                cards[j] = cards[i];
+                cards[i] = temp;
+            }
+        }
+    }
+}
+
+// Description: Compares card hand values. A hand is larger
+//              if the first character is larger then another hand.
+//              If characters are equal, the 2nd character is compared..
+//              Returns true if the first hand is greater.
+//              Otherwise returns false.
+bool compareHands(const string handOne, const string handTwo)
+{
+    for (int i = 0; i < handOne.length(); i++)
+    {
+        if(isFirstCharGreater(handOne[i], handTwo[i]) != -1){
+            return isFirstCharGreater(handOne[i], handTwo[i]);
+        }
+    }
+    return false; //base case; cards are equal.
+}
+
+// Description: Compares character values.
+//      Returns 1 if first digit is larger, 0 if 2nd digit is larger,
+//      -1 if they are equal.
+int isFirstCharGreater(const char digitOne, const char digitTwo)
+{
+    if (digitOne == digitTwo)
+        return -1; // equal
+
+    if (isdigit(digitOne) && isdigit(digitTwo))
+        return isdigit(digitOne) > isdigit(digitTwo);
+    else if (isdigit(digitTwo))
+    {
+        return 1;
+    }
+    else if (isdigit(digitOne))
+    {
+        return 0;
+    }
+    //else both number cards; A K Q J
+    char cardValues[4] = {'A', 'K', 'Q', 'J'};
+
+    for(int i = 0; i < 3; i ++){
+        if(digitOne == cardValues[i]){
+            return 1;
+        } else if(digitTwo == cardValues[i]){
+            return 0;
+        }
+    }
+    
 }
 
 int main()
@@ -54,7 +118,7 @@ int main()
     vector<string> fives, fours, fullhouses, threes, twos, ones, nones;
 
     string line;
-    while (getline(cin, line)) //reading input; placing it into proper bins
+    while (getline(cin, line)) // reading input; placing it into proper bins
     {
         string hand = line.substr(0, line.find(" "));
         int type = whatType(hand);
@@ -85,11 +149,12 @@ int main()
         }
     }
 
-    //TODO: Sort each group by strength (ascending). Start at 1s and add
-    //      product of value and bid.
+    // TODO: Sort each group by strength (ascending). Start at 1s and add
+    //       product of value and bid.
 
-    for(int i = 0; i < nones.size(); i++){
-        cout << nones[i] << endl; //testing - process input here
+    for (int i = 0; i < nones.size(); i++)
+    {
+        cout << nones[i] << endl; // testing - process input here
     }
 
     return 0;
