c548ff0142df8beab1356db6848001650a3092d1
Anton <antonilic7@gmail.com>
2023-12-31 16:21:27 -0800

    Completed OOB check function

--- AOC2023/day_16/main.cpp
+++ AOC2023/day_16/main.cpp
@@ -4,24 +4,25 @@
 #include <sstream>
 using namespace std;
 
-// Description: Checks if beam is out of bounds. If so, stops recursion.
-bool isOutOfBounds(int indexX, int indexY, int direction, vector<string> vec)
+// Description: Checks if beam is out of bounds.
+bool isOutOfBounds(int indexX, int indexY, const vector<string> &vec)
 {
-    // TODO: If OOB, return true.
+    if (0 <= indexX && indexX < vec.size() && 0 <= indexY && indexY < vec[indexX].length())
+        return false;
     return true;
 }
 
 /* Description: Recursive traversal of the beam from the specified index
                 to the specified direction.
-                Sets visitedSquares to be visited upon 
+                Sets visitedSquares to be visited upon
 */
-void traverse(int indexX, int indexY, int direction, vector<string> vec)
+void traverse(int indexX, int indexY, char direction, vector<string> vec)
 {
 
-    if (isOutOfBounds(indexX, indexY, direction, vec))
+    if (isOutOfBounds(indexX, indexY, vec))
         return;
     /*
-    
+
         if(vec[indexX][indexY] == "|" and direction is right or left)
             ...
         else if(vec[indexX][indexY] == "-" and direction is up or down
@@ -30,7 +31,7 @@ void traverse(int indexX, int indexY, int direction, vector<string> vec)
             ...
         else if(vec[indexX][indexY] == "/")
             ...
-        else 
+        else
             ... //is treated like a .; continue in direction.
 
     */
